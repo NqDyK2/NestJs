18fc13ad1fb659669cbb5b22a9af63af87eca9ba
Duy. Nguyen Quy - CMC Global DJ2 <nguyenquyduyabc@gmail.com>
2023-03-31 15:11:39 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    BookModule,
+    AuthModule,
+    ProductsModule,
+    CatsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
